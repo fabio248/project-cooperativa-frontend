cfd5a20d294603204df7cdd4072d6e901a048432
OscarRojas05 <lr18062@ues.edu.sv>
2023-02-12 12:05:28 -0600

    cambios menores

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -2,22 +2,54 @@ import React from "react";
 import './Navbar.css'; 
 const Navbar = () => {
     return(
-        <div className="container-fluid">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="navbar-brand" href="/">
-                        <img src="/src/assets/images/logo.png" alt="Bootstrap" width="50" height="50"></img>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="navbar-brand" href="/login">
-                            HOLA
-                        </a>
-                    </li>
-                </ul>
+        
+            <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+                <div className="container">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="navbar-brand" href="/">
+                            <img src="/src/assets/images/logo.png" alt="Bootstrap" width="60" height="60"></img>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul className="nav justify-content-end">
+                        <div className="derecha">
+                            <li className="nav-item">
+                                <a className="navbar-brand" href="/login">
+                                    LOGIN
+                                </a>
+                            </li>
+                        </div>
+                    </ul>
+                    <ul className="nav justify-content-end">
+                        <div className="derecha">
+                            <li className="nav-item">
+                                <a className="navbar-brand" href="/login">
+                                    ABOUT
+                                </a>
+                            </li>
+                        </div>
+                    </ul>
+                    <ul className="nav justify-content-end">
+                        <div className="derecha">
+                            <li className="nav-item">
+                                <a className="navbar-brand" href="/login">
+                                    WHAT WE DO
+                                </a>
+                            </li>
+                        </div>
+                    </ul>
+                    <ul className="nav justify-content-end">
+                        <div className="derecha">
+                            <li className="nav-item">
+                                <a className="navbar-brand" href="/login">
+                                    CONTACT
+                                </a>
+                            </li>
+                        </div>
+                    </ul>
+                </div>
             </nav>
-        </div>
     )
 };
 
